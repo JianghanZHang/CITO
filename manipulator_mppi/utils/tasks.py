6a357fed4958fa04147e5bc5f255c45905f8e779
JianghanZHang <zhangjianghan2@gmail.com>
2025-02-24 15:12:31 -0500

    remoced useless lines

--- manipulator_mppi/utils/tasks.py
+++ manipulator_mppi/utils/tasks.py
@@ -47,7 +47,6 @@ TASKS = {
     },
 
     "cube_manipulation": {
-
         # The center of the cube staying on the table is (0, 0, 0.013 = 0.125 + 0.005)
         "cube_state":[0, 0, 0.15, # Postion - x, y, z
                       0, 0, 0], # Orientation - roll, pitch, yaw
